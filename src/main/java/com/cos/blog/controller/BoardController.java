de98a60548c26c02eae3c2d8d599a07b1a6fe362
HJoo <yoon3798@naver.com>
2023-04-03 13:06:29 +0900

    글 목록 페이징

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.cos.blog.controller;
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +20,10 @@ public class BoardController {
     BoardService boardService;
 
     @GetMapping({"", "/"})
-    public String index(Model model){ // 데이터를 들고 view에 가야함
-        model.addAttribute("boards", boardService.글목록());
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){ // 데이터를 들고 view에 가야함
+
+
+        model.addAttribute("boards", boardService.글목록(pageable));
         return "index"; //viewResolver 작동하는데 모델 들고 감
     }
 
